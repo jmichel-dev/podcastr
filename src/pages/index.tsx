5fbfad83d9f9a1483a48e1d2301443eb7b3b0e06
Jean M S A Santos <jeaanmichel@protonmail.com>
2021-04-20 13:02:23 -0300

    home page definition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <h1>index</h1>
   )
+}
+
+// SSR - Server side rendering
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   };
+// }
+
+// SSG - Static Site generator
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // 8 horas
+  };
 }

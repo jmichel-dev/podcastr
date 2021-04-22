ce6ab471025228322e5de7c55ede4e2dc6acfde2
Jean M S A Santos <jeaanmichel@protonmail.com>
2021-04-21 23:54:56 -0300

    created podcast episode detail page

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,102 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router'
+import { api } from '../../services/api';
+import { convertDurationToTimeString } from '../../utils/converter';
+
+import styles from './episode.module.scss'
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  description: string;
+  thumbnail: string;
+  duration: number;
+  publishedAt: string;
+  url: string;
+  durationAsString: string;
+}
+
+type EpisodeProps = {
+  episode: Episode
+};
+
+export default function Episode({ episode }: EpisodeProps) {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.episode}>
+        <div className={styles.thumbnailContainer}>
+          <Link href="/">
+            <button type="button">
+              <img src="/arrow-left.svg" alt="Voltar" />
+            </button>
+          </Link>
+
+          <Image 
+            width={700}
+            height={360}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+
+          <button type="button">
+            <img src="/play.svg" alt="Tocar podcast"/>
+          </button>
+        </div>
+
+        <header>
+          <h1>{episode.title}</h1>
+          <span>{episode.members}</span>
+          <span>{episode.publishedAt}</span>
+          <span>{episode.durationAsString}</span>
+        </header>
+
+        <div 
+          className={styles.description} 
+          dangerouslySetInnerHTML={{ __html: episode.description }} 
+        />
+      </div>
+    </div>
+  );
+}
+
+// Dinamic SSG - Static Site Generation
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+}
+
+// SSG - Static Site Generation
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+  const { data } = await api.get(`/episodes/${slug}`);
+
+  const episode: Episode = {
+    id: data.id,
+    title: data.title,
+    members: data.members,
+    thumbnail: data.thumbnail,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
+      locale: ptBR,
+    }),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    duration: Number(data.file.duration),
+    description: data.description,
+    url: data.file.url,
+  }
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
+}
